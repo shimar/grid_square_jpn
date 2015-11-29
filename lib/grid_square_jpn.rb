c129dc7cbfc3b85040b6f9b2b0f39e6e3ac8bb38
shimar <shima01dev@gmail.com>
2015-11-29 16:28:25 +0900

    add yardoc comment.

--- lib/grid_square_jpn.rb
+++ lib/grid_square_jpn.rb
@@ -2,18 +2,48 @@ require "grid_square_jpn/version"
 
 module GridSquareJpn
 
+  #
+  # get the mesh code of primary area partition.
+  # @param  [Float]  lat  latitude.
+  # @param  [Float]  lng  longitude.
+  # @return [String] the mesh code.
+  #
   def primary(lat, lng)
   end
 
+  #
+  # get the mesh code of secondary area partition.
+  # @param  [Float]  lat  latitude.
+  # @param  [Float]  lng  longitude.
+  # @return [String] the mesh code.
+  #
   def secondary(lat, lng)
   end
 
+  #
+  # get the mesh code of basic grid square.
+  # @param  [Float]  lat  latitude.
+  # @param  [Float]  lng  longitude.
+  # @return [String] the mesh code.
+  #
   def basic(lat, lng)
   end
 
+  #
+  # get the mesh code of half grid square.
+  # @param  [Float]  lat  latitude.
+  # @param  [Float]  lng  longitude.
+  # @return [String] the mesh code.
+  #
   def half(lat, lng)
   end
 
+  #
+  # get the mesh code of quarter grid square.
+  # @param  [Float]  lat  latitude.
+  # @param  [Float]  lng  longitude.
+  # @return [String] the mesh code.
+  #
   def quarter(lat, lng)
   end
 
