347a0fff9562ee49cc45a4e213cf3d97f4c2ea58
shimar <shima01dev@gmail.com>
2015-11-29 16:13:41 +0900

    add some methods.

--- lib/grid_square_jpn.rb
+++ lib/grid_square_jpn.rb
@@ -1,5 +1,20 @@
 require "grid_square_jpn/version"
 
 module GridSquareJpn
-  # Your code goes here...
+
+  def primary(lat, lng)
+  end
+
+  def secondary(lat, lng)
+  end
+
+  def basic(lat, lng)
+  end
+
+  def half(lat, lng)
+  end
+
+  def quarter(lat, lng)
+  end
+
 end
