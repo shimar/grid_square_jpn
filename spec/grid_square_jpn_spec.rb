f136cdbc2889e34872041a7dbd18ccb630759f19
shimar <shima01dev@gmail.com>
2015-12-01 10:33:45 +0900

    write the spec of the validate method.

--- spec/grid_square_jpn_spec.rb
+++ spec/grid_square_jpn_spec.rb
@@ -4,4 +4,28 @@ describe GridSquareJpn do
   it 'has a version number' do
     expect(GridSquareJpn::VERSION).not_to be nil
   end
+
+
+  describe "validate" do
+    context "when the lat is less than 20.0," do
+      it "raise OutOfRangeException."
+    end
+
+    context "when the lat is greater than 46.0," do
+      it "raise OutOfRangeException."
+    end
+
+    context "when the lng is less than 122.0," do
+      it "raise OutOfRangeException."
+    end
+
+    context "when the lng is greater than 154.0," do
+      it "raise OutOfRangeException."
+    end
+
+    context "when valid lat and lng given," do
+      it "returns true."
+    end
+  end
+
 end
