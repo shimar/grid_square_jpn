66fd7ef8464eded468512d3c9c1fe2be876b70e8
shimar <shima01dev@gmail.com>
2016-01-18 21:03:23 +0900

    add :sw, :se, :nw, :ne methods to Bound class.

--- lib/grid_square_jpn/bound.rb
+++ lib/grid_square_jpn/bound.rb
@@ -14,5 +14,37 @@ module GridSquareJpn
       @east  = east
       @west  = west
     end
+
+    #
+    # Return the Point of south-west.
+    # @return [Point] the south-west point.
+    #
+    def sw
+      Point.new south, west
+    end
+
+    #
+    # Return the Point of south-east.
+    # @return [Point] the south-east point.
+    #
+    def se
+      Point.new south, east
+    end
+
+    #
+    # Return the Point of north-west.
+    # @return [Point] the north-west point.
+    #
+    def nw
+      Point.new north, west
+    end
+
+    #
+    # Return the Point of north-east.
+    # @return [Point] the north-east point.
+    #
+    def ne
+      Point.new north, east
+    end
   end
 end
--- spec/grid_square_jpn/bound_spec.rb
+++ spec/grid_square_jpn/bound_spec.rb
@@ -12,4 +12,60 @@ describe GridSquareJpn::Bound do
     end
   end
 
+  describe "sw" do
+    before do
+      @bound = GridSquareJpn::Bound.new(north: 46.0, south: 20.0, east: 154.0, west: 122.0)
+    end
+
+    it "returns an object of Point." do
+      expect(@bound.sw).to be_instance_of GridSquareJpn::Point
+    end
+
+    it "returns the point of south-west." do
+      expect(@bound.sw.to_a).to eq [ 20.0, 122.0 ]
+    end
+  end
+
+  describe "se" do
+    before do
+      @bound = GridSquareJpn::Bound.new(north: 46.0, south: 20.0, east: 154.0, west: 122.0)
+    end
+
+    it "returns an object of Point." do
+      expect(@bound.se).to be_instance_of GridSquareJpn::Point
+    end
+
+    it "returns the point of south-east." do
+      expect(@bound.se.to_a).to eq [ 20.0, 154.0 ]
+    end
+  end
+
+  describe "nw" do
+    before do
+      @bound = GridSquareJpn::Bound.new(north: 46.0, south: 20.0, east: 154.0, west: 122.0)
+    end
+
+    it "returns an object of Point." do
+      expect(@bound.nw).to be_instance_of GridSquareJpn::Point
+    end
+
+    it "returns the point of north-west." do
+      expect(@bound.nw.to_a).to eq [ 46.0, 122.0 ]
+    end
+  end
+
+  describe "ne" do
+    before do
+      @bound = GridSquareJpn::Bound.new(north: 46.0, south: 20.0, east: 154.0, west: 122.0)
+    end
+
+    it "returns an object of Point." do
+      expect(@bound.ne).to be_instance_of GridSquareJpn::Point
+    end
+
+    it "returns the point of north-east." do
+      expect(@bound.ne.to_a).to eq [ 46.0, 154.0 ]
+    end
+  end
+
 end
