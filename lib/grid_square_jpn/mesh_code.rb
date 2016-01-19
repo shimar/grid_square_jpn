042357c1fceab68232ebb5dcb14090cb1eafd276
shimar <shima01dev@gmail.com>
2016-01-19 21:25:14 +0900

    define some methods of MeshCode.

--- lib/grid_square_jpn/mesh_code.rb
+++ lib/grid_square_jpn/mesh_code.rb
@@ -15,5 +15,27 @@ module GridSquareJpn
       @half      = half
       @quarter   = quarter
     end
+
+    #
+    # build an MeshCode object by code string.
+    #
+    # @param [String] code a string of mesh code.
+    # @return [MeshCode] a instance of MeshCode.
+    #
+    def self.build code
+      # TODO
+    end
+
+    private
+
+    #
+    # return whether the code is vaild.
+    #
+    # @param [String] code a string of mesh code.
+    # @return [Boolean] true if valid.
+    #
+    def self.valid_code? code
+      # TODO
+    end
   end
 end
--- spec/grid_square_jpn/mesh_code_spec.rb
+++ spec/grid_square_jpn/mesh_code_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 describe GridSquareJpn::MeshCode do
+
+  describe "build" do
+    it "returns new instance of MeshCode."
+    context "when the given code is invalid," do
+      # TODO
+    end
+  end
+
+  describe "valid_code?" do
+    it "returns true if the code is valid."
+    it "returns false if the code is invalid."
+  end
+
 end
